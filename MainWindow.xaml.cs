e5d1b7153f52e857d65383fae5adf3e598f743dc
xin <654244035@qq.com>
2019-12-25 11:10:42 +0800

    init

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Forms;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+
+namespace kaoshi
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private Email _mail;
+        public MainWindow()
+        {
+            InitializeComponent();
+            _mail = new Email();
+            this.DataContext = _mail;
+        }
+
+   
+
+        private void ShowFile(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog
+            {
+                Multiselect = false,//该值确定是否可以选择多个文件
+                Title = "请选择文件夹",
+                Filter = "所有文件(*.*)|*.*"
+            };
+            if (dialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                _mail.PictureFileName = dialog.FileName;
+            }
+        }
+
+        private void GetZhengWen(object sender, RoutedEventArgs e)
+        {
+            string filePath="";
+            OpenFileDialog dialog = new OpenFileDialog
+            {
+                Multiselect = false,//该值确定是否可以选择多个文件
+                Title = "请选择文件夹",
+                Filter = "所有文件(*.*)|*.*"
+            };
+            if (dialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                filePath = dialog.FileName;
+            }
+            else
+            {
+                System.Windows.MessageBox.Show("文件不存在");
+            }
+            //文件路径
+            
+ 
+            if (File.Exists(filePath))
+                {
+                    string contents = File.ReadAllText(filePath);
+                    _mail.Zhengwen = contents;
+            }
+            
+     
+        }
+
+        private void CheckAll(object sender, RoutedEventArgs e)
+        {
+            Regex regex = new Regex(@"[a-zA-Z0-9]+@[a-zA-z]+\.com$");
+            MatchCollection matchs = regex.Matches(_mail.Dress);
+            if (!regex.IsMatch(_mail.Dress)) {
+                System.Windows.MessageBox.Show("邮件名错误！");
+            }
+            else if(_mail.Zhengwen==string.Empty)
+            {
+                System.Windows.MessageBox.Show("未输入正文");
+            }
+            else if (_mail.PictureFileName==string.Empty)
+            {
+                System.Windows.MessageBox.Show("未插入图片");
+            }
+            else {
+                System.Windows.MessageBox.Show("已完成");
+            }
+        }
+    }
+}
---
+++ email.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+
+namespace kaoshi
+{
+    class Email : INotifyPropertyChanged
+    {
+        private string dre = "";
+        private string data = "";
+        private string image_dre = "";
+        public event PropertyChangedEventHandler PropertyChanged;
+        public string Dress
+        {
+            get { return dre; }
+            set
+            {
+                dre = value;
+            }
+        }
+        public string Zhengwen
+        {
+            get { return data; }
+            set
+            {
+                if (data == value) return;
+                data = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Zhengwen"));
+            }
+        }
+        public string PictureFileName
+        {
+            get { return image_dre; }
+            set
+            {
+                if (image_dre == value) return;
+                image_dre = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("PictureFileName"));
+            }
+        }
+
+    }
+}
